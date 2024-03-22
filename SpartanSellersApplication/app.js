323ea0baab23f8c540c22d2763745ea939831c4a
meghan wilcox <meghanwilcox4235@gmail.com>
2024-03-21 22:17:13 -0400

    meghan - deleted db.js, fixed db connection

--- SpartanSellersApplication/app.js
+++ SpartanSellersApplication/app.js
@@ -10,7 +10,7 @@ app.use(cors()); // Enable CORS
 
 app.use(bodyParser.json());
 
-
+//getDBConnection: a function to establish a connection with the database
 async function getDBConnection() {
     const db = await sqlite.open({
     filename: 'data/spartanSellersDatabase.db',
@@ -19,26 +19,10 @@ async function getDBConnection() {
     return db;
 }
 
-
 // Root endpoint
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
-
-app.get('/categories', async function (req, res) {
-    try {
-        let qry = 'SELECT categoryName FROM category;';
-        let db = await getDBConnection();
-        let categories = await db.all(qry);
-        await db.close();
-        console.log('Categories fetched successfully:', categories); // Log the fetched categories
-        res.send(categories); // Send the categories as JSON response
-    } catch (err) {
-        console.error('Error fetching categories:', err);
-        res.status(500).json({ error: 'Internal Server Error' }); // Send an error response
-    }
-});
-
 
 // Start the server
 app.listen(3000, function() {
